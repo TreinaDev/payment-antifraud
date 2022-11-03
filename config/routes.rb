dc523b33a9c2b6cfcb452cd853ddb45c854fbd54
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-11-03 17:43:00 -0300

    Configura o I18n no model promo

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   devise_for :admins
+<<<<<<< HEAD
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+=======
+>>>>>>> Configura o I18n no model promo
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "home#index"
+  resources :promos, only: [:index, :new, :create]
 end
