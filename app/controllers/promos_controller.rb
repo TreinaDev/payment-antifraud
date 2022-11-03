1148f07d7d087dd9d7a59ce2aa74f8b5fd212b93
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-11-03 18:46:47 -0300

    Faz o create de promo, validações e testes Co-authored

--- app/controllers/promos_controller.rb
+++ app/controllers/promos_controller.rb
@@ -7,5 +7,24 @@ class PromosController < ApplicationController
     @promo = Promo.new
   end
 
+  def create
+    @promo = Promo.new(promo_params)
+    if @promo.save 
+      redirect_to promo_path(@promo.id), notice: 'Promoção cadastrada com sucesso!'
+    else 
+      flash.now[:notice] = 'Não foi possível cadastrar a promoção'
+      render 'new'
+    end
+  end
+
+  def show
+    @promo = Promo.find(params[:id])
+  end
+
   
+  private
+  
+  def promo_params
+    params.require(:promo).permit(:name, :starting_date, :discount_max, :usages_max, :product_list, :ending_date, :discount_percentage)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
 >>>>>>> Configura o I18n no model promo
 
   root to: "home#index"
-  resources :promos, only: [:index, :new, :create]
+  resources :promos, only: [:index, :new, :create, :show]
 end
