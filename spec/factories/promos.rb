5b2f257070c31e53422ed7a2cffcf4863903d973
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-11-03 17:43:56 -0300

    adiciona teste do index e cadastro

--- spec/factories/promos.rb
+++ spec/factories/promos.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
   factory :promo do
     starting_date { "2022-11-01" }
     ending_date { "2022-11-01" }
-    name { "MyString" }
-    discount_percentage { 1 }
-    discount_max { 1 }
+    name { 'Black Friday' }
+    discount_percentage { 5 }
+    discount_max { 50 }
     product_list { "MyString" }
-    usages_max { 1 }
-    voucher { "MyString" }
+    usages_max { 10 }
+    voucher { "Black123" }
   end
 end
