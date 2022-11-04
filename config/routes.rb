0cc2fdf46044fa4cee073f9f7513af2c29d1d982
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-11-04 18:58:40 -0300

    remove root duplicado

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :admins
   root 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  root to: "home#index"
   resources :promos, only: [:index, :new, :create, :show, :edit, :update]
 end
