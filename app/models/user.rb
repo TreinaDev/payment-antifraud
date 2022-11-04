f46bddff222b228ee29438562692ca26e48601f8
minccia <lusyvasilva@gmail.com>
2022-11-04 17:00:56 -0300

    Adicionado status de aprovado no enum do usuário

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  enum status: { pending: 0 }
+  enum status: { pending: 0, approved: 1 }
 end
