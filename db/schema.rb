4973ae1c1a8787d6144e8b7416f44bf7215b14d7
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-11-04 19:15:41 -0300

    adiciona a migration user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_01_204100) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_03_202610) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -65,6 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_01_204100) do
     t.string "registration_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
