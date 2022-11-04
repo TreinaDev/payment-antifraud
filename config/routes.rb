66f8ff9fd4dd29ebf5fd9547924e4af30b4e1342
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-11-04 18:58:08 -0300

    resolve coinflito routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   devise_for :admins
-<<<<<<< HEAD
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-=======
->>>>>>> Configura o I18n no model promo
 
   root to: "home#index"
   resources :promos, only: [:index, :new, :create, :show, :edit, :update]
