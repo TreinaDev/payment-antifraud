57dd420a377bb155454cf403b154cbfa6569ee6b
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-11-08 16:21:15 -0300

    novo método no model promo Co-authored-by: ana-borowsky <ana-borowsky@users.noreply.github.com>

--- app/models/promo.rb
+++ app/models/promo.rb
@@ -1,15 +1,19 @@
 class Promo < ApplicationRecord
   validates :name, :starting_date, :discount_max, :usages_max, :product_list, :ending_date, :discount_percentage,
             presence: true
+  validates :discount_max, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :usages_max, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :discount_percentage, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+
   validate :ending_date_greater_than_starting_date
   before_validation :generate_voucher, on: :create
 
   private
 
   def ending_date_greater_than_starting_date
-    if !self.starting_date.nil? && !self.ending_date.nil? && self.starting_date > self.ending_date
-      errors.add(:ending_date, I18n.t('activerecord.errors.models.promo.attributes.ending_date.date_error'))
-    end
+    return unless starting_date && ending_date && starting_date > ending_date
+
+    errors.add(:ending_date, I18n.t('activerecord.errors.models.promo.attributes.ending_date.date_error'))
   end
 
   def generate_voucher
