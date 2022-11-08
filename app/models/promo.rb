13248d9f6671b9c0bffa400abffdbc9791c1c00a
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-11-08 11:47:28 -0300

    Validates no model da data inicial e final Co-authored-by: ana-borowsky <ana-borowsky@users.noreply.github.com>

--- app/models/promo.rb
+++ app/models/promo.rb
@@ -1,9 +1,16 @@
 class Promo < ApplicationRecord
   validates :name, :starting_date, :discount_max, :usages_max, :product_list, :ending_date, :discount_percentage,
             presence: true
+  validate :ending_date_greater_than_starting_date
   before_validation :generate_voucher, on: :create
 
   private
+
+  def ending_date_greater_than_starting_date
+    if self.starting_date > self.ending_date
+      errors.add(:ending_date, I18n.t('activerecord.errors.models.promo.attributes.ending_date.date_error'))
+    end
+  end
 
   def generate_voucher
     self.voucher = SecureRandom.alphanumeric(8).upcase
