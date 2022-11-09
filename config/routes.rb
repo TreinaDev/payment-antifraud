10b82ba8fa014f9fd5447ab0ffbf27cb1f7daf3d
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-11-09 14:16:22 -0300

    Ajusta as rotas e o form de promo conforme review o-authored-by: ana-borowsky <ana-borowsky@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :admins 
   root 'home#index'
 
-  resources :promos, except: [:destroy]
+  resources :promos, only: [:index, :new, :create, :show, :edit, :update]
 
   resources :payment_methods, only: [:index, :new, :create, :show, :edit, :update]
 
