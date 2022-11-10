a147aabe7b783d2579d1a8d743cdf93c249fc4dc
Thaís A <thais.antero@gmail.com>
2022-11-10 19:44:53 -0300

    Corrige erro de formatação

--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :invoice do
     insurance_company_id { Faker::Number.number(digits: 2) }
     package_id { Faker::Number.number(digits: 2) }
-    registration_number {  Faker::Number.number(digits: 8) }
+    registration_number { Faker::Number.number(digits: 8) }
     status { 0 }
   end
 end
