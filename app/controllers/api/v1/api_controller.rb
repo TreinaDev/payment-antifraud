f9d476368d0a40d4a2d6ee0b925ef4ff1c8206f7
Thaís A <thais.antero@gmail.com>
2022-11-12 00:58:32 -0300

    Ajustes do rubocop

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,12 +1,18 @@
-class Api::V1::ApiController < ActionController::API
-  rescue_from ActiveRecord::ActiveRecordError, with: :return_500
-  rescue_from ActiveRecord::RecordNotFound, with: :return_404
+module Api
+  module V1
+    class ApiController < ActionController::API
+      rescue_from ActiveRecord::ActiveRecordError, with: :internal_server_error_status
+      rescue_from ActiveRecord::RecordNotFound, with: :not_found_status
 
-  private
-  def return_500
-    render status: 500
-  end
-  def return_404
-    render status: 404
+      private
+
+      def internal_server_error_status
+        render status: :internal_server_error
+      end
+
+      def not_found_status
+        render status: :not_found
+      end
+    end
   end
 end
--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -1,27 +1,30 @@
-class Api::V1::InvoicesController < Api::V1::ApiController
-  def index
-    invoices = Invoice.all.order(order_id: :desc)
-    render status: :ok, json: invoices.as_json
-  end
+module Api
+  module V1
+    class InvoicesController < Api::V1::ApiController
+      def index
+        invoices = Invoice.all.order(order_id: :desc)
+        render status: :ok, json: invoices.as_json
+      end
 
-  def show
-    invoice = Invoice.find(params[:id])
-    render status: :ok, json: invoice.as_json(except: %i[created_at updated_at])
-  end
+      def show
+        invoice = Invoice.find(params[:id])
+        render status: :ok, json: invoice.as_json(except: %i[created_at updated_at])
+      end
 
-  def create
-    invoice = Invoice.new(invoice_params)
-    if invoice.save
-      render status: :created, json: invoice.as_json
-    else
-      render status: :precondition_failed, json: { errors: invoice.errors.full_messages }
+      def create
+        invoice = Invoice.new(invoice_params)
+        if invoice.save
+          render status: :created, json: invoice.as_json
+        else
+          render status: :precondition_failed, json: { errors: invoice.errors.full_messages }
+        end
+      end
+
+      private
+
+      def invoice_params
+        params.require(:invoice).permit(:order_id, :insurance_company_id, :package_id, :registration_number)
+      end
     end
   end
-
-  private
-
-  def invoice_params
-    params.require(:invoice).permit(:order_id, :insurance_company_id, :package_id, :registration_number)
-  end
-
 end
