2002b133e176d7a93ccb8f7edd75ddbe23a2cb29
Bruna Barboza Vieira <bruna.barboza111@gmail.com>
2022-11-14 18:01:56 -0300

    Adicionada API para consumir meios de pagamento de uma seguradora
    Co-authored-by: minccia <lusyva.pro@gmail.com>

---
+++ app/controllers/api/v1/api_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::ApiController < ActionController::API
+  rescue_from ActiveRecord::ActiveRecordError, with: :return500
+  rescue_from ActiveRecord::RecordNotFound, with: :return404
+
+  private
+
+  def return404
+    render status: :not_found, body: {}
+  end
+
+  def return500
+    render status: :internal_server_error, body: {}
+  end
+end
---
+++ app/controllers/api/v1/insurance_companies_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::InsuranceCompaniesController < Api::V1::ApiController
+  def payment_options
+    company = InsuranceCompany.find_by(external_insurance_company: params[:id])
+    return render status: :ok, json: create_json_response(company) if company.present?
+
+    raise ActiveRecord::RecordNotFound
+  end
+
+  private
+
+  def create_json_response(company)
+    company.payment_options.map do |po|
+      {
+        name: po.payment_method.name,
+        payment_type: po.payment_method.payment_type,
+        tax_percentage: po.payment_method.tax_percentage,
+        tax_maximum: po.payment_method.tax_maximum,
+        max_parcels: po.max_parcels,
+        single_parcel_discount: po.single_parcel_discount
+      }
+    end
+  end
+end
