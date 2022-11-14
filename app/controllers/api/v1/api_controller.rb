3126487ae3297ee6b3d7ae4362b9fe18b5800848
Bruna Barboza Vieira <bruna.barboza111@gmail.com>
2022-11-14 18:40:02 -0300

    Pequenos ajustes para se adequar ao rubocop

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,14 +1,18 @@
-class Api::V1::ApiController < ActionController::API
-  rescue_from ActiveRecord::ActiveRecordError, with: :return500
-  rescue_from ActiveRecord::RecordNotFound, with: :return404
+module Api
+  module V1
+    class ApiController < ActionController::API
+      rescue_from ActiveRecord::ActiveRecordError, with: :return500
+      rescue_from ActiveRecord::RecordNotFound, with: :return404
 
-  private
+      private
 
-  def return404
-    render status: :not_found, body: {}
-  end
+      def return404
+        render status: :not_found, body: {}
+      end
 
-  def return500
-    render status: :internal_server_error, body: {}
+      def return500
+        render status: :internal_server_error, body: {}
+      end
+    end
   end
 end
