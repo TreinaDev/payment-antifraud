6e66f1c3ca6dc5e40438430ebdbf51f4a0bce504
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2022-11-15 18:21:14 -0300

    muda exibiçao de json para incluir lista de produtos e filtro para nao exibir promoçoes sem produtos
    Co-authored-by: petragrunheidt <petragrunheidt@gmail.com>

--- app/controllers/api/v1/promos_controller.rb
+++ app/controllers/api/v1/promos_controller.rb
@@ -2,13 +2,29 @@ module Api
   module V1
     class PromosController < Api::V1::ApiController
       def index
-        promos = Promo.all
-        render status: :ok, json: promos
+        promos = Promo.all.select { |promo| promo.promo_products.present? }
+        render status: :ok, json: json_format(promos)
       end
 
-      def show
-        promo = Promo.find(params[:id])
-        render status: :ok, json: promo
+      private
+
+      # rubocop:disable Metrics/MethodLength
+      def json_format(promo)
+        promo.map do |p|
+          {
+            id: p.id,
+            name: p.name,
+            starting_date: p.starting_date,
+            ending_date: p.ending_date,
+            usages_max: p.usages_max,
+            discount_percentage: p.discount_percentage,
+            discount_max: p.discount_max,
+            voucher: p.voucher,
+            insurance_company_id: p.insurance_company_id,
+            products: p.promo_products.map(&:product_id)
+          }
+        end
+        # rubocop:enable Metrics/MethodLength
       end
     end
   end
--- spec/requests/apis/promo_api_spec.rb
+++ spec/requests/apis/promo_api_spec.rb
@@ -1,51 +1,22 @@
 require 'rails_helper'
 
 describe 'Promo API' do
-  context 'GET /api/v1/promos/1' do
-    it 'com sucesso' do
-      company = FactoryBot.create(:insurance_company)
-      allow(SecureRandom).to receive(:alphanumeric).and_return('3MVGTOVW')
-      promo = FactoryBot.create(:promo, name: 'Black Friday', starting_date: Time.zone.today,
-                                        ending_date: Time.zone.today + 30.days,
-                                        discount_max: 100, discount_percentage: 20, usages_max: 10,
-                                        insurance_company_id: company.id)
-
-      get "/api/v1/promos/#{promo.id}"
-
-      expect(response.status).to eq 200
-      expect(response.content_type).to include 'application/json'
-      json_response = JSON.parse(response.body)
-      expect(json_response['name']).to eq('Black Friday')
-      expect(json_response['starting_date']).to eq Time.zone.today.strftime('%Y-%m-%d')
-      expect(json_response['ending_date']).to eq (Time.zone.today + 30.days).strftime('%Y-%m-%d')
-      expect(json_response['discount_max']).to eq 100
-      expect(json_response['discount_percentage']).to eq 20
-      expect(json_response['usages_max']).to eq 10
-      expect(json_response['insurance_company_id']).to eq company.id
-      expect(json_response['voucher']).to eq '3MVGTOVW'
-    end
-
-    it 'e falha se a promoção não for encontrada' do
-      get '/api/v1/promos/999999999'
-
-      expect(response.status).to eq 404
-    end
-  end
-
   context 'GET /api/v1/promos/' do
     it 'com sucesso' do
       company = FactoryBot.create(:insurance_company)
       allow(SecureRandom).to receive(:alphanumeric).and_return('3MVGTOVW')
-      FactoryBot.create(:promo, name: 'Black Friday', starting_date: Time.zone.today + 7.days,
-                                ending_date: Time.zone.today + 30.days,
-                                discount_max: 100, discount_percentage: 20, usages_max: 10,
-                                insurance_company_id: company.id)
+      promo_a = FactoryBot.create(:promo, name: 'Black Friday', starting_date: Time.zone.today + 7.days,
+                                          ending_date: Time.zone.today + 30.days,
+                                          discount_max: 100, discount_percentage: 20, usages_max: 10,
+                                          insurance_company_id: company.id)
 
       promo_b = FactoryBot.create(:promo, name: 'Promo Natal', starting_date: Time.zone.today,
                                           ending_date: Time.zone.today + 20.days,
                                           discount_max: 500, discount_percentage: 10, usages_max: 15,
                                           insurance_company_id: company.id)
-      promo_b.update(voucher: '3MVGTOVW')
+      promo_b.update(voucher: 'TOVW3MVG')
+      FactoryBot.create(:promo_product, promo: promo_a, product_id: 3)
+      FactoryBot.create(:promo_product, promo: promo_b, product_id: 5)
 
       get '/api/v1/promos/'
 
@@ -61,6 +32,7 @@ describe 'Promo API' do
       expect(json_response[0]['usages_max']).to eq 10
       expect(json_response[0]['insurance_company_id']).to eq company.id
       expect(json_response[0]['voucher']).to eq '3MVGTOVW'
+      expect(json_response[0]['products']).to eq [3]
       expect(json_response[1]['name']).to eq('Promo Natal')
       expect(json_response[1]['starting_date']).to eq Time.zone.today.strftime('%Y-%m-%d')
       expect(json_response[1]['ending_date']).to eq (Time.zone.today + 20.days).strftime('%Y-%m-%d')
@@ -68,7 +40,38 @@ describe 'Promo API' do
       expect(json_response[1]['discount_percentage']).to eq 10
       expect(json_response[1]['usages_max']).to eq 15
       expect(json_response[1]['insurance_company_id']).to eq company.id
-      expect(json_response[1]['voucher']).to eq '3MVGTOVW'
+      expect(json_response[1]['voucher']).to eq 'TOVW3MVG'
+      expect(json_response[1]['products']).to eq [5]
+    end
+    it 'não exibe promoções sem produtos cadastrados' do
+      company = FactoryBot.create(:insurance_company)
+      allow(SecureRandom).to receive(:alphanumeric).and_return('3MVGTOVW')
+      promo_a = FactoryBot.create(:promo, name: 'Black Friday', starting_date: Time.zone.today + 7.days,
+                                          ending_date: Time.zone.today + 30.days,
+                                          discount_max: 100, discount_percentage: 20, usages_max: 10,
+                                          insurance_company_id: company.id)
+
+      FactoryBot.create(:promo, name: 'Promo Natal', starting_date: Time.zone.today,
+                                ending_date: Time.zone.today + 20.days,
+                                discount_max: 500, discount_percentage: 10, usages_max: 15,
+                                insurance_company_id: company.id)
+      FactoryBot.create(:promo_product, promo: promo_a, product_id: 3)
+
+      get '/api/v1/promos/'
+
+      expect(response.status).to eq 200
+      expect(response.content_type).to include 'application/json'
+      json_response = JSON.parse(response.body)
+      expect(json_response.length).to eq 1
+      expect(json_response[0]['name']).to eq('Black Friday')
+      expect(json_response[0]['starting_date']).to eq (Time.zone.today + 7.days).strftime('%Y-%m-%d')
+      expect(json_response[0]['ending_date']).to eq (Time.zone.today + 30.days).strftime('%Y-%m-%d')
+      expect(json_response[0]['discount_max']).to eq 100
+      expect(json_response[0]['discount_percentage']).to eq 20
+      expect(json_response[0]['usages_max']).to eq 10
+      expect(json_response[0]['insurance_company_id']).to eq company.id
+      expect(json_response[0]['voucher']).to eq '3MVGTOVW'
+      expect(json_response[0]['products']).to eq [3]
     end
 
     it 'e retorna vazio se não houver promoções cadastradas' do
