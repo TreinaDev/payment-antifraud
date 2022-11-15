77de2e901ebae26a3d736e7feec05b9c44f2b5ce
Thaís A <thais.antero@gmail.com>
2022-11-15 19:56:41 -0300

    Adiciona validação do meio de pagamento relativo a seguradora e o teste
    Co-authored-by: Thais <thais.antero@gmail.com>

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,10 +3,22 @@ class Invoice < ApplicationRecord
   before_validation :generate_token, on: :create
   belongs_to :payment_method
   belongs_to :insurance_company
+  validate :check_payment_method_options
 
   private
 
   def generate_token
     self.token = SecureRandom.alphanumeric(20).upcase
+  end
+
+  def check_payment_method_options
+    return if insurance_company.blank?
+
+    return if insurance_company.payment_options.empty?
+    if insurance_company.payment_options.any? { |payment_option| payment_option.payment_method == payment_method }
+      return
+    end
+
+    errors.add(:payment_method, 'deve ter meio de pagamento escolhido por seguradora')
   end
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,10 +1,16 @@
-  require 'rails_helper'
+require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
   describe 'Gera um código aleatório' do
     it 'ao criar um uma cobrança' do
       payment_method = create(:payment_method)
       insurance_company = create(:insurance_company)
+      user = create(:user, insurance_company:)
+      create(:company_payment_option, user:,
+                                      insurance_company:,
+                                      payment_method:,
+                                      max_parcels: 10,
+                                      single_parcel_discount: 5)
       invoice = build(:invoice, payment_method_id: payment_method.id, insurance_company_id: insurance_company.id)
 
       invoice.save!
@@ -17,11 +23,40 @@ RSpec.describe Invoice, type: :model do
     it 'e o código é único' do
       payment_method = create(:payment_method)
       insurance_company = create(:insurance_company)
+      user = create(:user, insurance_company:)
+      create(:company_payment_option, user:,
+                                      insurance_company:,
+                                      payment_method:,
+                                      max_parcels: 10,
+                                      single_parcel_discount: 5)
       invoice_a = create(:invoice, payment_method_id: payment_method.id, insurance_company_id: insurance_company.id)
       invoice_b = create(:invoice, payment_method_id: payment_method.id, insurance_company_id: insurance_company.id)
 
       result = invoice_a.token
       expect(invoice_b.token).not_to eq result
+    end
+  end
+
+  describe '#valid?' do
+    it 'com meio de pagamento não escolhido pela seguradora' do
+      payment_method1 = create(:payment_method, name: 'Laranja',
+                                                tax_percentage: 5, tax_maximum: 100,
+                                                payment_type: 'Cartão de Crédito',
+                                                status: :active)
+      insurance_company = create(:insurance_company)
+      user = create(:user, insurance_company:)
+      create(:company_payment_option, user:,
+                                      insurance_company:,
+                                      payment_method: payment_method1,
+                                      max_parcels: 10,
+                                      single_parcel_discount: 5)
+      payment_method2 = create(:payment_method, name: 'Amarelo',
+                                                tax_percentage: 10, tax_maximum: 80,
+                                                payment_type: 'Boleto',
+                                                status: :active)
+      invoice = build(:invoice, payment_method: payment_method2, insurance_company:)
+
+      expect(invoice.valid?).to be false
     end
   end
 end
