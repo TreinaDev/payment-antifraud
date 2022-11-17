e914767199c4d89e10ba460d0d05e3579293013a
Bruna Barboza Vieira <bruna.barboza111@gmail.com>
2022-11-17 17:48:36 -0300

    add coluna voucher em invoices e ajusta testes
    Co-authored-by: Luiza Rodrigues <luiza.oceanoufc@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_15_182705) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_17_191712) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_15_182705) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -89,6 +89,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_15_182705) do
     t.integer "order_id"
     t.integer "payment_method_id"
     t.integer "insurance_company_id"
+    t.string "voucher"
     t.index ["insurance_company_id"], name: "index_invoices_on_insurance_company_id"
     t.index ["payment_method_id"], name: "index_invoices_on_payment_method_id"
   end
--- spec/system/invoice/user_view_invoices_details_spec.rb
+++ spec/system/invoice/user_view_invoices_details_spec.rb
@@ -9,7 +9,7 @@ describe 'Usuário vê detalhes de uma cobranças' do
     FactoryBot.create(:company_payment_option, insurance_company_id: company.id,
                                                payment_method_id: payment_method.id, user:)
     invoice = create(:invoice, payment_method:, insurance_company_id: company.id, package_id: 10,
-                               registration_number: '12345678', status: :pending)
+                               registration_number: '12345678', status: :pending, voucher: 'Black123')
 
     login_as(user, scope: :user)
     visit root_path
@@ -21,9 +21,10 @@ describe 'Usuário vê detalhes de uma cobranças' do
     expect(current_path).to eq invoice_path(invoice.id)
     expect(page).to have_content 'Status: pendente'
     expect(page).to have_content 'AGBS65OFN493OE93MVNA'
-    expect(page).to have_content 'ID Seguradora: 1'
-    expect(page).to have_content 'ID Pacote de Seguros: 10'
+    expect(page).to have_content "ID Seguradora: #{invoice.insurance_company_id}"
+    expect(page).to have_content "ID Pacote de Seguros: #{invoice.package_id}"
     expect(page).to have_content 'Meio de Pagamento:'
     expect(page).to have_content 'Tipo de Pagamento:'
+    expect(page).to have_content "Cupom: #{invoice.voucher}"
   end
 end
