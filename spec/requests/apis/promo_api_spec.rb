6453d0af3866e6fe706d88599a3b552e241d5b04
Petra <petragrunheidt@gmail.com>
2022-11-17 10:20:57 -0300

    adiciona teste para duas situaçoes de valor
    Co-authored-by anavonborowsky <anavonborowsky@gmail.com>

--- spec/requests/apis/promo_api_spec.rb
+++ spec/requests/apis/promo_api_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'Promo API' do
   context 'GET /api/v1/promos/voucher' do
-    it 'e acessa um cupom válido' do
+    it 'e acessa um cupom válido, cujo desconto é o valor máximo de desconto' do
       company = FactoryBot.create(:insurance_company)
       allow(SecureRandom).to receive(:alphanumeric).and_return('3MVGTOVW')
       promo_a = FactoryBot.create(:promo, name: 'Black Friday', starting_date: Time.zone.today - 7.days,
@@ -20,6 +20,24 @@ describe 'Promo API' do
       expect(json_response['discount']).to eq(60)
     end
 
+    it 'e acessa um cupom válido, cujo desconto é menor que o valor máximo de desconto' do
+      company = FactoryBot.create(:insurance_company)
+      allow(SecureRandom).to receive(:alphanumeric).and_return('3MVGTOVW')
+      promo_a = FactoryBot.create(:promo, name: 'Black Friday', starting_date: Time.zone.today - 7.days,
+                                          ending_date: Time.zone.today + 30.days,
+                                          discount_max: 200, discount_percentage: 20, usages_max: 10,
+                                          insurance_company_id: company.id)
+      FactoryBot.create(:promo_product, promo: promo_a, product_id: 3)
+
+      get '/api/v1/promos/3-3MVGTOVW-500'
+
+      expect(response.status).to eq 200
+      expect(response.content_type).to include 'application/json'
+      json_response = JSON.parse(response.body)
+      expect(json_response['status']).to eq('Cupom válido.')
+      expect(json_response['discount']).to eq(100)
+    end
+
     it 'e retorna não encontrado ao digitar um cupom não cadastrado' do
       get '/api/v1/promos/DIAOSMDK20329203'
 
@@ -34,7 +52,7 @@ describe 'Promo API' do
       expect(response).to have_http_status(500)
     end
 
-    it 'cupom é válido e está expirado por data' do
+    it 'cupom existe e está expirado por data' do
       company = FactoryBot.create(:insurance_company)
       allow(SecureRandom).to receive(:alphanumeric).and_return('3MVGTOVW')
       promo_a = FactoryBot.create(:promo, name: 'Black Friday', starting_date: Time.zone.today - 30.days,
@@ -51,7 +69,7 @@ describe 'Promo API' do
       expect(json_response['status']).to eq('Cupom expirado.')
     end
 
-    it 'cupom é válido e promoção tem data futura' do
+    it 'cupom existe e promoção tem data futura' do
       company = FactoryBot.create(:insurance_company)
       allow(SecureRandom).to receive(:alphanumeric).and_return('3MVGTOVW')
       promo_a = FactoryBot.create(:promo, name: 'Promo Petra', starting_date: Time.zone.today + 30.days,
@@ -68,7 +86,7 @@ describe 'Promo API' do
       expect(json_response['status']).to eq('Cupom inválido.')
     end
 
-    it 'cupom é válido e promoção não é valida para o produto buscado' do
+    it 'cupom existe e promoção não é valida para o produto buscado' do
       company = FactoryBot.create(:insurance_company)
       allow(SecureRandom).to receive(:alphanumeric).and_return('3MVGTOVW')
       promo_a = FactoryBot.create(:promo, name: 'Promo Petra', starting_date: Time.zone.today - 30.days,
