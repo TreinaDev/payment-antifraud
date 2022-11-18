32c7b43fcc32d0775982b3acfcfb6643a5325454
minccia <lusyvasilva@gmail.com>
2022-11-18 18:10:47 -0300

    Inicia implementacao do create de fraud_report
    Co-authored-by: ana-borowsky <anavonborowsky@gmail.com>

--- app/models/fraud_report.rb
+++ app/models/fraud_report.rb
@@ -1,4 +1,6 @@
 class FraudReport < ApplicationRecord
   enum status: { pending: 0, denied: 3, confirmed_fraud: 5 }
   has_many_attached :images
+  validates :registration_number, presence: true
+  validates :registration_number, length: { is: 11 }
 end
