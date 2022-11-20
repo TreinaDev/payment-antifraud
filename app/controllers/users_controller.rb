50627582c8fdb9efda4fb4dc483884f914fd7509
minccia <lusyvasilva@gmail.com>
2022-11-19 21:41:40 -0300

    Adiciona select para filtrar lista de usuarios no index de users
    Co-authored-by: ana-borowsky <anavonborowsky@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,11 @@ class UsersController < ApplicationController
   before_action :require_admin
 
   def index
-    @users = User.all.sort_by(&:status)
+    @filter_options = { pending: 'Usuários pendentes', approved: 'Usuários aprovados', refused: 'Usuários reprovados' }
+    @users = if params[:filter_option].present?
+               User.where(status: @filter_options.key(params[:filter_option]))
+             else
+               User.all.sort_by(&:status)
+             end
   end
 end
