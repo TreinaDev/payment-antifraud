3ac9d331a5108950ed02f003acb60b85cda5cf1c
minccia <lusyvasilva@gmail.com>
2022-11-20 01:15:33 -0300

    Atualiza seeds

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     email { "#{SecureRandom.alphanumeric(8)}@paolaseguros.com.br" }
     password { 'password' }
-    name { 'Luis Inacio Lula da Silva' }
+    name { Faker::Name.name }
     registration_number { '39439419203' }
     status { :approved }
     after(:build) do |user|
