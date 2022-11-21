15062bd6f3bae03fe13b5c8802fd427180cf3dce
minccia <lusyvasilva@gmail.com>
2022-11-21 17:09:11 -0300

    Adiciona checagem de imagens nos testes de sistema
    Co-authored-by: ana-borowsky <anavonborowsky@gmail.com>

--- spec/system/fraud_reports/user_view_fraud_report_details_spec.rb
+++ spec/system/fraud_reports/user_view_fraud_report_details_spec.rb
@@ -19,8 +19,12 @@ describe 'Usuário vê detalhes de uma denúncia de fraude' do
     user = FactoryBot.create(:user, insurance_company_id: company.id)
     fraud = FactoryBot.create(
       :fraud_report, insurance_company_id: company.id,
-                     registration_number: '12345678911', description: 'É CALOTEIRO',
-                     status: :pending
+                     registration_number: '12345678911', description: 'Tentou fraudar o seguro.',
+                     status: :pending,
+                     images: [
+                              Rack::Test::UploadedFile.new(Rails.root.join('spec/support/crime.jpeg')),
+                              Rack::Test::UploadedFile.new(Rails.root.join('spec/support/fotos_do_crime.jpeg'))
+                             ]
     )
 
     login_as user, scope: :user
@@ -28,8 +32,10 @@ describe 'Usuário vê detalhes de uma denúncia de fraude' do
 
     expect(current_path).to eq fraud_report_path(fraud.id)
     expect(page).to have_content 'Denúncia do CPF: 123.456.789-11'
-    expect(page).to have_content 'Descrição: É CALOTEIRO'
+    expect(page).to have_content 'Descrição: Tentou fraudar o seguro.'
     expect(page).to have_content 'Status: Pendente'
+    expect(page).to have_css('img[src*="crime.jpeg"]')
+    expect(page).to have_css('img[src*="fotos_do_crime.jpeg"]')
     expect(page).not_to have_button 'Aprovar'
     expect(page).not_to have_button 'Reprovar'
   end
