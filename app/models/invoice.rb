ebe51f07709301ef0b4a93c4921c9ae9849ad483
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-11-23 16:59:50 -0300

    Corrige bug na criação da invoice
    Co-authored-by: brunabarbozavieira <bruna.barboza111@gmail.com>

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,8 +3,9 @@ class Invoice < ApplicationRecord
   before_validation :generate_token, on: :create
   belongs_to :payment_method
   belongs_to :insurance_company
-
-  validate :check_payment_method_options
+  
+  validate :valid_insurance_company?
+  validate :check_payment_method_options, if: :valid_insurance_company?
   validates :transaction_registration_number, presence: true, on: :update, if: :approved?
   validates :reason_for_failure, presence: true, on: :update, if: :refused?
 
@@ -27,10 +28,15 @@ class Invoice < ApplicationRecord
   def generate_token
     self.token = SecureRandom.alphanumeric(20).upcase
   end
-
+  
   def check_payment_method_options
     return if insurance_company.payment_options.map(&:payment_method).include?(payment_method)
 
     errors.add(:payment_method, 'deve ter meio de pagamento escolhido por seguradora')
+  end
+  
+  def valid_insurance_company?
+    return true if InsuranceCompany.find_by(external_insurance_company: insurance_company_id)
+    false
   end
 end
