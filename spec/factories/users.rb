3de852053cbd8e1e212a9172f6ad3aee903e5458
minccia <lusyvasilva@gmail.com>
2022-11-24 20:47:32 -0300

    Altera validacao do email do usuario para bater em outro endpoint da API de seguradoras

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,10 +5,5 @@ FactoryBot.define do
     name { Faker::Name.name }
     registration_number { (11.times.map { rand(1..9) }).join }
     status { :approved }
-    after(:build) do |user|
-      class << user
-        def consult_insurance_company_api_for_email_validation; end
-      end
-    end
   end
 end
