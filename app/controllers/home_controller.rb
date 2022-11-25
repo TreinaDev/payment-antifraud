7d597547feaa3b0456f3224c41dfa73df3b68e68
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2022-11-24 21:18:26 -0300

    Alterações mínimas no layout e ajuste do design da homepage
    co-authored-by: petragrunheidt <petragrunheidt@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,17 @@
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @insurance_companies_sample = get_insurance_companies_logos
+    binding.b
+  end
+
+  private
+
+  def get_insurance_companies_logos
+    insurance_url = "#{Rails.configuration.external_apis['insurance_api']}/insurance_companies"
+    response = Faraday.get(insurance_url)
+    return [] if response.status == 204
+    raise ActiveRecord::QueryCanceled if response.status == 500
+    data = JSON.parse(response.body)
+    data.sample(3).map! { |d| {logo: d["logo_url"], name: d["name"]} }
+  end
 end
