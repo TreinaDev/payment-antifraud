2176d9c9d88c00e8f4e7d8d834c94ffefa89018e
minccia <lusyvasilva@gmail.com>
2022-11-26 18:22:10 -0300

    Corrige bug nas opcoes de pagamento da seguradora

--- app/controllers/company_payment_options_controller.rb
+++ app/controllers/company_payment_options_controller.rb
@@ -2,18 +2,23 @@ class CompanyPaymentOptionsController < ApplicationController
   include Pagination
 
   before_action :require_user
+  before_action :fetch_company_payment_options, only: %i[index show]
   before_action :fetch_payment_option, only: %i[show edit update destroy]
 
   def index
-    @payment_options = current_user.insurance_company.payment_options
-    @payment_methods = PaymentMethod.active.where.not(id: CompanyPaymentOption.all.pluck(:payment_method_id))
+    @payment_options = @company_payment_options
+    @payment_methods = PaymentMethod.active.where.not(id: @company_payment_options.pluck(:payment_method_id))
     @pagination, @payment_options = paginate(
-      collection: current_user.insurance_company.payment_options,
+      collection: @company_payment_options,
       params: page_params(10)
     )
   end
 
-  def show; end
+  def show
+    unless @company_payment_options.include? @payment_option
+      return redirect_to root_path, alert: t('no_access_granted')
+    end
+  end
 
   def new
     @payment_option = CompanyPaymentOption.new(payment_method_id: params[:payment_method_id])
@@ -50,6 +55,10 @@ class CompanyPaymentOptionsController < ApplicationController
   end
 
   private
+
+  def fetch_company_payment_options
+    @company_payment_options = current_user.insurance_company.payment_options
+  end
 
   def fetch_payment_option
     @payment_option = CompanyPaymentOption.find params[:id]
